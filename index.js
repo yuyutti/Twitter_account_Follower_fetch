2c40a2f10f75766846a5e0acfbc722caf52705ad
yuyutti <yuyutti@yuyutti.com>
2023-09-09 21:35:10 +0900

    commits

--- index.js
+++ index.js
@@ -24,10 +24,7 @@ app.get('/user', async(req,res) => {
 
 async function followers(username){
     try{
-        const browser = await puppeteer.launch({
-            args: ['--no-sandbox']
-        });
-        
+        const browser = await puppeteer.launch({ headless: false, headless: "new" });
         const page = await browser.newPage();
     
         await login(page)
